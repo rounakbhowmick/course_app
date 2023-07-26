35a7056b1fdd742d185c0ec761538a2631d73385
rounak-yubi <rounak.bhowmick@go-yubi.com>
2023-07-26 16:26:24 +0530

    added controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'enrollments/index'
+  get 'enrollments/new'
+  get 'enrollments/create'
+  get 'enrollments/destroy'
+  get 'courses/index'
+  get 'students/index'
+  get 'students/new'
+  get 'students/create'
+  get 'students/show'
+  get 'students/edit'
+  get 'students/update'
+  get 'students/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
